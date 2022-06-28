17e8c18bc71935e32d8974cb50672ddbcc31a483
Artyom Valevich <apelsinoid@icloud.com>
2022-06-28 05:30:33 +0300

    123123

---
+++ Areas/Identity/Pages/Chat/Chat.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MinaCoolProgekt.Areas.Identity.Pages.Chat;
+
+public class Chat : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Hubs/ProjectChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MinaCoolProgekt.Hubs;
+
+public class ProjectChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
